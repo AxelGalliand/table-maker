d7dc331d145c2dfb650122a2dc8b6d7ee212a1b7
Axel Galliand <galliandaxel88@gmail.com>
2024-03-11 22:12:13 +0100

    accessibility improvement

--- src/Component/Table/tableMaker.jsx
+++ src/Component/Table/tableMaker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import styles from '../../styles/tableMaker.module.css';
 import TableContent from './TableContent';
 import PropTypes from 'prop-types';
@@ -25,12 +25,10 @@ const TableMaker = (props) => {
       setColumns(columnsUpdated)
     }
 
-
     const SearchBar = (e) => {
       const input = e.target.value.toLowerCase();
       setSearch(input)
     }
-
 
     const filteredData = data.filter((row) => {
       return Object.keys(row).some((key) => row[key].toLowerCase().includes(search))
@@ -56,6 +54,11 @@ const TableMaker = (props) => {
     const indexFirstData = indexLastData - slice
     const currentData = sortedDatas.slice(indexFirstData, indexLastData)
     const totalPage = Math.ceil(sortedDatas.length / slice)
+    const SliceIssues = () => {
+      if(currentPage > totalPage){
+        setCurrentPage(totalPage)
+      }
+    }
     const SliceBy = (e) => {
       const number = +e.target.value
       setSlice(number)
@@ -64,12 +67,10 @@ const TableMaker = (props) => {
     const range = (from, to, step = 1) => {
       let i = from;
       const range = [];
-    
       while (i <= to) {
         range.push(i);
         i += step;
       }
-    
       return range;
     }
 
@@ -92,71 +93,70 @@ const TableMaker = (props) => {
     const nextPage = () => {
       setCurrentPage(currentPage + 1)
     }
-
-    
+    useEffect(SliceIssues,[slice])
     return (
         
-        <div className={styles["TableDiv"]}>
-         
-          <div className={styles["paginateBlock"]}>
-            <input type='text' className={styles["TableSerch"]} placeholder='Research' onChange={SearchBar}></input>
-            <div className={styles["block-right"]}>
-              <div className={styles["sizeButton"]}>
-                <select className={styles["sizeSelect"]} onChange={SliceBy}>
-                  <option value={10}>10</option>
-                  <option value={25}>25</option>
-                  <option value={50}>50</option>
-                  <option value={100}>100</option>
-                </select>
-              </div>
-              <div className={styles["paginate"]}>
+      <div className={styles["TableDiv"]}>
+       
+        <div className={styles["paginateBlock"]}>
+          <input type='text' className={styles["TableSerch"]} placeholder='Research' onChange={SearchBar}></input>
+          <div className={styles["block-right"]}>
+            <div className={styles["sizeButton"]}>
+              <select className={styles["sizeSelect"]} aria-label="selector" onChange={SliceBy}> 
+                <option value={10}>10</option>
+                <option value={25}>25</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
+              </select>
+            </div>
+            <div className={styles["paginate"]}>
 
-                <button onClick={() => firstPage()} className={currentPage === 1 ? 'active' : null} >1</button>
-                {currentPage === 1 ? null : <button onClick={() => previousPage()}><svg className={styles['svg']} height="1em" viewBox="0 0 512 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></button>}
+              <button onClick={() => firstPage()} className={currentPage === 1 ? 'active' : null} >1</button>
+              {currentPage === 1 ? null : <button onClick={() => previousPage()}><svg className={styles['svg']} aria-label="previous button" height="1em" viewBox="0 0 512 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></button>}
 
-                {range(2, totalPage-1).map((numberPage) => {
-                  if (numberPage === currentPage-2 || numberPage === currentPage-1 || numberPage === currentPage || numberPage === currentPage+1 || numberPage === currentPage+2) {
-                    return <button key={numberPage} onClick={() => changePage(numberPage)} className={currentPage === numberPage ? 'active' : null}> {numberPage}</button>}
-                })}
-                {currentPage === totalPage ? null : <button onClick={() => nextPage()} ><svg className={styles["svg"]} height="16" viewBox="0 0 512 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></button>}
-                {totalPage === 1 ? null : <button onClick={() => lastPage()} className={currentPage === totalPage ? 'active' : null}>{totalPage}</button>}
-              </div>
+              {range(2, totalPage-1).map((numberPage) => {
+                if (numberPage === currentPage-2 || numberPage === currentPage-1 || numberPage === currentPage || numberPage === currentPage+1 || numberPage === currentPage+2) {
+                  return <button key={numberPage} onClick={() => changePage(numberPage)} className={currentPage === numberPage ? 'active' : null}> {numberPage}</button>}
+              })}
+              {currentPage === totalPage ? null : <button onClick={() => nextPage()} ><svg className={styles["svg"]} aria-label="next button" height="16" viewBox="0 0 512 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></button>}
+              {totalPage === 1 ? null : <button onClick={() => lastPage()} className={currentPage === totalPage ? 'active' : null}>{totalPage}</button>}
             </div>
           </div>
-          <div className={styles["TableContener"]}>
-            <table className={styles["Table"]}>
-              <thead className={styles["TableThead"]}>
-                <tr>
-                  {columns.map((column) => <th className={styles["columnTitle"]} key={column.dataField} onClick={() => sortRow(column)}>
-                    <div className={styles["TableTheadTitle"]}>
-                    <span>{column.text}</span>
-                     {column.sortOrder === "ASC" ? <svg className={styles['svg']} height="1em" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> : <svg className={styles['svg']} height="1em" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>}
-                     </div>
-                  </th>)}
-                </tr>
-              </thead>
-              <TableContent columns={columns} data={currentData} />
-            </table>  
-          </div>
-          <div className={styles["paginate"]}>
-
-            <button onClick={() => firstPage()} className={currentPage === 1 ? 'active' : null}>1</button>
-            {currentPage === 1 ? null : <button onClick={() => previousPage()}><svg className={styles['svg']} height="1em" viewBox="0 0 512 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></button>}
-
-            {range(2, totalPage-1).map((numberPage) => {
-              if (numberPage === currentPage-2 || numberPage === currentPage-1 || numberPage === currentPage || numberPage === currentPage+1 || numberPage === currentPage+2) {
-                return <button key={numberPage} onClick={() => changePage(numberPage)} className={currentPage === numberPage ? 'active' : null}> {numberPage}</button>}
-            })}
-            {currentPage === totalPage ? null : <button onClick={() => nextPage()} ><svg className={styles["svg"]} height="16" viewBox="0 0 512 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></button>}
-            {totalPage === 1 ? null : <button onClick={() => lastPage()} className={currentPage === totalPage ? 'active' : null}>{totalPage}</button>}
-          </div>
-
         </div>
-    )
+        <div className={styles["TableContener"]}>
+          <table className={styles["Table"]}>
+            <thead className={styles["TableThead"]}>
+              <tr>
+                {columns.map((column) => <th className={styles["columnTitle"]} key={column.dataField} onClick={() => sortRow(column)}>
+                  <div className={styles["TableTheadTitle"]}>
+                  <span>{column.text}</span>
+                   {column.sortOrder === "ASC" ? <svg className={styles['svg']} height="1em" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> : <svg className={styles['svg']} height="1em" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>}
+                   </div>
+                </th>)}
+              </tr>
+            </thead>
+            <TableContent columns={columns} data={currentData} />
+          </table>  
+        </div>
+        <div className={styles["paginate"]}>
+
+          <button onClick={() => firstPage()} className={currentPage === 1 ? 'active' : null}>1</button>
+          {currentPage === 1 ? null : <button onClick={() => previousPage()}><svg className={styles['svg']} aria-label="previous button" height="1em" viewBox="0 0 512 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></button>}
+
+          {range(2, totalPage-1).map((numberPage) => {
+            if (numberPage === currentPage-2 || numberPage === currentPage-1 || numberPage === currentPage || numberPage === currentPage+1 || numberPage === currentPage+2) {
+              return <button key={numberPage} onClick={() => changePage(numberPage)} className={currentPage === numberPage ? 'active' : null}> {numberPage}</button>}
+          })}
+          {currentPage === totalPage ? null : <button onClick={() => nextPage()} ><svg className={styles["svg"]} aria-label="next button" height="16" viewBox="0 0 512 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></button>}
+          {totalPage === 1 ? null : <button onClick={() => lastPage()} className={currentPage === totalPage ? 'active' : null}>{totalPage}</button>}
+        </div>
+
+      </div>
+  )
 }
- TableMaker.propTypes ={
-  rows: PropTypes.array,
-  columns: PropTypes.array
- }
+TableMaker.propTypes ={
+rows: PropTypes.array,
+columns: PropTypes.array
+}
 
 export default TableMaker
